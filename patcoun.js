8f38efe78a93dbd8626688c839e2f4d5e06280a6
Paul Nwodo <145398513+Tonypaul544@users.noreply.github.com>
2024-06-02 21:12:54 +0100

    Add files via upload

---
+++ patcoun.js
@@ -0,0 +1,15 @@
+
+// document.getElementById("decrease").onclick = function(){
+//   count-=7 
+//  document.getElementById("value").innerHTML = count;
+// }
+
+document.getElementById("start").onclick = function(){
+ count=3
+document.getElementById("value").innerHTML = count;
+}
+
+document.getElementById("increase").onclick = function(){
+ count+=2
+document.getElementById("value").innerHTML = count;
+}
